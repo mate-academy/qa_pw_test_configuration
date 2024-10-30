275b46002ac02d944b557a4023c8a472f595b4cd
Inna Ianko <inna.i@mate.academy>
2024-10-30 17:05:57 +0200

    Make sign up stable

--- tests/signUp/signUpPositive.spec.js
+++ tests/signUp/signUpPositive.spec.js
@@ -4,12 +4,14 @@ import { SignUpPage } from '../../src/pages/SignUpPage';
 import { HomePage } from '../../src/pages/HomePage'; 
 
 test('Successful `Sign up` flow test', async ({ page }) => {
+  const userName = `${faker.person.firstName()}${faker.person.lastName()}`
+  const email = `${faker.person.firstName()}${faker.internet.email()}`
   const signUpPage = new SignUpPage(page); 
   const homePage = new HomePage(page); 
 
   await signUpPage.open();
-  await signUpPage.fillUsernameField(faker.person.firstName());
-  await signUpPage.fillEmailField(faker.internet.email());
+  await signUpPage.fillUsernameField(userName);
+  await signUpPage.fillEmailField(email);
   await signUpPage.fillPasswordField(faker.internet.password());
   await signUpPage.clickSignUpButton(); 
 
